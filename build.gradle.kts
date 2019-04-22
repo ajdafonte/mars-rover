ffeb421b4e403c68f31173164e5feaacc685f6d2
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2019-04-22 22:49:46 +0200

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    java
+    application
+    id("org.springframework.boot") version "2.1.4.RELEASE"
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    group = "pt.caires"
+    version = "0.0.1-SNAPSHOT"
+    mainClassName = "pt.caires.MarsRoverApplication"
+}
+
+repositories {
+    mavenCentral()
+}
+
+ext {
+    // IMPORTANT: Override JUnit BOM from spring dependency management
+    // See: https://stackoverflow.com/questions/54598484/gradle-5-junit-bom-and-spring-boot-incorrect-versions
+    set("junit-jupiter.version", "5.4.0")
+}
+
+dependencies {
+    implementation("org.springframework.boot", "spring-boot-starter-web")
+
+    // testing
+    testImplementation("org.springframework.boot", "spring-boot-starter-test") {
+        // Exclude JUnit 4 from starter-test, because it will be used JUnit 5
+        exclude(group = "junit")
+        exclude(group = "org.mockito")
+        exclude(group = "org.hamcrest")
+    }
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.4.0")
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.4.0")
+    // IMPORTANT: Use this version. Why? See https://github.com/mockito/mockito/issues/1604
+    testImplementation("org.mockito", "mockito-junit-jupiter", "2.23.4")
+    testImplementation("org.hamcrest", "hamcrest-library", "1.3")
+}
